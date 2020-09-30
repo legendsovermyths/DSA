8148401940071998bb9eebcb5f479c1b1f2f7624
Mask Wolf <maskwolf2000@gmail.com>
2020-10-01 03:07:31 +0530

    linked list

---
+++ JavaScript/SinglyLinkedList.js
@@ -0,0 +1,13 @@
+class node {
+  constructor(val, next) {
+    val = this.val;
+    next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+}
