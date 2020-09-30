c39492bc1f2c4f476e6de7608d4d38f2866f564a
Mask Wolf <maskwolf2000@gmail.com>
2020-10-01 03:31:25 +0530

    add push

--- JavaScript/SinglyLinkedList.js
+++ JavaScript/SinglyLinkedList.js
@@ -1,13 +1,30 @@
-class node {
-  constructor(val, next) {
-    val = this.val;
-    next = null;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = null;
+    this.length = 0;
+  }
+  push(val) {
+    var node = new Node(val);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      this.length += 1;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+      this.length += 1;
+    }
   }
 }
+list = new SinglyLinkedList();
+list.push(12);
+list.push(13);
+list.push(16);
+console.log(list);
