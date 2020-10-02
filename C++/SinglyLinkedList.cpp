29b61001c18388a3eb3fa97b8694d58711802b85
Mask Wolf <maskwolf2000@gmail.com>
2020-10-03 01:29:43 +0530

    linked list print function added

--- C++/SinglyLinkedList.cpp
+++ C++/SinglyLinkedList.cpp
@@ -3,50 +3,51 @@ using namespace std;
 
 class Node
 {
-public:
-    int value;
-    Node *next;
-    Node()
-    {
-        next = nullptr;
-    }
-    Node(int val)
-    {
-        value = val;
-        next = nullptr;
-    }
-};
 
+public:
+    int data;
+    Node *link = NULL;
+};
 class SinglyLinkedList
 {
 public:
-    Node head;
-    Node tail;
+    Node *head = new Node();
+    Node *tail = new Node();
+
     int length = 0;
-    SinglyLinkedList(){};
     void push(int val)
     {
-        Node newNode = Node(val);
-        if (head.next == nullptr)
+        Node *newNode = new Node();
+        if (head->link == NULL)
         {
+            newNode->data = val;
             head = newNode;
-            head.next = &newNode;
-            tail = newNode;
+            tail = head;
+            head->link = tail;
             length += 1;
         }
         else
         {
-            tail.next = &newNode;
+            newNode->data = val;
+            tail->link = newNode;
             tail = newNode;
             length += 1;
+        }
+    }
+    void printList(Node *n)
+    {
+        while (n != NULL)
+        {
+            cout << n->data << " ";
+            n = n->link;
         }
     }
 };
 int main()
 {
-    SinglyLinkedList list1 = SinglyLinkedList();
-    list1.push(12);
-    list1.push(13);
-    list1.push(15);
-    cout << list1.head.next->value << endl;
+    SinglyLinkedList l1;
+    l1.push(10);
+    l1.push(11);
+    l1.push(13);
+    l1.printList(l1.head);
 }
