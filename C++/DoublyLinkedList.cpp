57646e80c50238142479ff743c3e083a5946d5ca
Mask Wolf <maskwolf2000@gmail.com>
2020-10-06 01:41:50 +0530

    doubly linked list added

---
+++ C++/DoublyLinkedList.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next = NULL;
+    Node *prev = NULL;
+};
+class DoublyLinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+    int length = 0;
+    void push(int val)
+    {
+        Node *newNode = new Node();
+        if (length == 0)
+        {
+            newNode->data = val;
+            head = newNode;
+            tail = head;
+            length += 1;
+        }
+        else
+        {
+            newNode->data = val;
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+            length += 1;
+        }
+    }
+    void printForward(Node *n)
+    {
+        while (n != NULL)
+        {
+            cout << n->data << " ";
+            n = n->next;
+        }
+    }
+    void printBackward(Node *n)
+    {
+        while (n != NULL)
+        {
+            cout << n->data << " ";
+            n = n->prev;
+        }
+    }
+    void pop()
+    {
+        tail = tail->prev;
+        tail->next = NULL;
+        length -= 1;
+    }
+    void shift()
+    {
+        head = head->next;
+        head->prev = NULL;
+        length -= 1;
+    }
+    void unshift(int val)
+    {
+        Node *newNode = new Node();
+        newNode->data = val;
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        length += 1;
+    }
+};
+int main()
+{
+    DoublyLinkedList list;
+    for (int i = 0; i < 25; i++)
+    {
+        list.push(i);
+    }
+    list.printForward(list.head);
+    cout << "\n";
+    list.pop();
+    list.printForward(list.head);
+    cout << "\n";
+    list.shift();
+    list.printForward(list.head);
+    cout << "\n";
+    list.unshift(0);
+    list.printForward(list.head);
+}
